073fd1437714c4da984fcc9894e15f9d93c28949
Jerome Mondol <jeromemondol11@gmail.com>
2025-09-30 00:55:43 +0600

    basic ARD-ESP com

---
+++ arduino/arduino.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+
+#define SLAVE_ADDR 0x08  // I2C address of Arduino
+
+void setup() {
+  Wire.begin(SLAVE_ADDR); // Join I2C bus as slave
+  Wire.onRequest(sendData); // Callback when master requests data
+  Wire.onReceive(receiveData); // Callback when master sends data
+  Serial.begin(115200);
+}
+
+void loop() {
+  // You can do sensor/motor stuff here
+  delay(100);
+}
+
+void sendData() {
+  Wire.write("HELLO ROAM"); // Send message to ESP
+}
+
+void receiveData(int byteCount) {
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c); // Print received chars
+  }
+  Serial.println();
+}
---
+++ nodemcu/nodemcu.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+
+#define SLAVE_ADDR 0x08
+
+void setup() {
+  Wire.begin(); // Master mode
+  Serial.begin(115200);
+}
+
+void loop() {
+  // Request data from Arduino
+  Wire.requestFrom(SLAVE_ADDR, 20); // request up to 20 bytes
+  while (Wire.available()) {
+    char c = Wire.read();
+    Serial.print(c);
+  }
+  Serial.println();
+
+  // Example: send a command to Arduino
+  Wire.beginTransmission(SLAVE_ADDR);
+  Wire.write("F"); // e.g., Forward command
+  Wire.endTransmission();
+
+  delay(1000);
+}
