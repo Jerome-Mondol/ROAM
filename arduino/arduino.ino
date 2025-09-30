c34b857d9d5c09c61817315c89427fcc5767c6ae
Jerome Mondol <jeromemondol11@gmail.com>
2025-09-30 12:14:29 +0600

    simple data sending

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,43 @@
+#include <Wire.h>
 #include "motorcontrol.h"
+
+#define SLAVE_ADDR 0x08
 
 Motor motors(10, 5, 9, 8, 7, 6); // ENA, ENB, IN1, IN2, IN3, IN4
 
 void setup() {
   motors.motorSetup();
-  motors.setSpeed(200, 180);
-  motors.testMotors();
+  Wire.begin(SLAVE_ADDR);          // Arduino = I2C slave
+  Wire.onReceive(receiveEvent);    // callback when master sends data
+  Serial.begin(115200);
+  Serial.println("Arduino I2C slave ready");
 }
 
 void loop() {
-  
+  // Nothing needed here; I2C handled in receiveEvent
+}
+
+// I2C callback
+void receiveEvent(int bytes) {
+  String cmd = "";
+  while (Wire.available()) {
+    char c = Wire.read();
+    cmd += c;
+  }
+  cmd.trim();  // remove whitespace
+
+  // Print received command
+  Serial.print("Received via I2C (");
+  Serial.print(cmd.length());
+  Serial.print(" chars): ");
+  Serial.println(cmd);
+
+  // Execute motor commands
+  if (cmd == "FORWARD") motors.forward();
+  else if (cmd == "BACKWARD") motors.backward();
+  else if (cmd == "LEFT") motors.turnLeft();
+  else if (cmd == "RIGHT") motors.turnRight();
+  else if (cmd == "STOP") motors.stop();
+
+  delay(10); // tiny delay to prevent overlapping I2C events
 }
--- nodemcu/nodemcu.ino
+++ nodemcu/nodemcu.ino
@@ -1,25 +1,49 @@
 #include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
 
 #define SLAVE_ADDR 0x08
 
+const char* ssid = "ROAM_WIFI";
+const char* password = "12345678";
+
+ESP8266WebServer server(80);
+
 void setup() {
-  Wire.begin(); // Master mode
   Serial.begin(115200);
+  Wire.begin(D2, D1); // SDA=D2, SCL=D1 on ESP8266
+  WiFi.softAP(ssid, password);
+  Serial.println("Web server started!");
+
+  server.on("/", handleRoot);
+  server.on("/forward", [](){ sendCommand("FORWARD"); });
+  server.on("/backward", [](){ sendCommand("BACKWARD"); });
+  server.on("/left", [](){ sendCommand("LEFT"); });
+  server.on("/right", [](){ sendCommand("RIGHT"); });
+  server.on("/stop", [](){ sendCommand("STOP"); });
+
+  server.begin();
 }
 
 void loop() {
-  // Request data from Arduino
-  Wire.requestFrom(SLAVE_ADDR, 20); // request up to 20 bytes
-  while (Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c);
-  }
-  Serial.println();
+  server.handleClient();
+}
 
-  // Example: send a command to Arduino
+void handleRoot() {
+  String html = "<h1>ROAM Control</h1>"
+                "<button onclick=\"fetch('/forward')\">Forward</button>"
+                "<button onclick=\"fetch('/backward')\">Backward</button>"
+                "<button onclick=\"fetch('/left')\">Left</button>"
+                "<button onclick=\"fetch('/right')\">Right</button>"
+                "<button onclick=\"fetch('/stop')\">Stop</button>";
+  server.send(200, "text/html", html);
+}
+
+void sendCommand(String cmd) {
   Wire.beginTransmission(SLAVE_ADDR);
-  Wire.write("Hello from esp"); // e.g., Forward command
+  Wire.write(cmd.c_str(), cmd.length()); // send exact chars
   Wire.endTransmission();
 
-  delay(1000);
+  Serial.println("Sent: " + cmd);
+  server.send(200, "text/plain", "Command sent: " + cmd);
 }
